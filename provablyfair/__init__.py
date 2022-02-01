f0515e8525791aac10b9d33b79d20084dd9e7164
Ryu juheon <saidbysolo@gmail.com>
2022-02-01 12:37:31 +0900

    feat: add generate_server_seed func

---
+++ provablyfair/__init__.py
@@ -0,0 +1,10 @@
+from typing import Tuple
+from secrets import token_hex
+from hashlib import sha256
+
+
+def generate_server_seed() -> Tuple[str, str]:
+    server_seed = token_hex(20)
+    server_seed_hash_object = sha256(server_seed.encode())
+    server_seed_hash = server_seed_hash_object.hexdigest()
+    return server_seed, server_seed_hash
