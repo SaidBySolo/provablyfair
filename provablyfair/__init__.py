a41700ad762cc3152ec1e693df57c73a48ab6c05
Ryu juheon <saidbysolo@gmail.com>
2022-02-02 13:35:11 +0900

    feat: hash_server_seed func

--- provablyfair/__init__.py
+++ provablyfair/__init__.py
@@ -8,3 +8,9 @@ def generate_server_seed() -> Tuple[str, str]:
     server_seed_hash_object = sha256(server_seed.encode())
     server_seed_hash = server_seed_hash_object.hexdigest()
     return server_seed, server_seed_hash
+
+
+def hash_server_seed(server_seed: str):
+    server_seed_hash_object = sha256(server_seed.encode())
+    server_seed_hash = server_seed_hash_object.hexdigest()
+    return server_seed, server_seed_hash
